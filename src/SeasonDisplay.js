e40d343c8edc06e0eceaf89a6c3828ac33e291ea
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-03 18:15:13 +0530

    logic to getSeason

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,28 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        if (lat > 0) {
+            //northern hemisphere
+            return 'summer';
+        } else {
+            //southern hemisphere
+            return 'winter';
+        }
+    } else {
+        if (lat > 0) {
+            return 'winter';
+        } else {
+            return 'summer';
+        }
+    }
+}
+
+const SeasonDisplay = (props) => {
+
+    const season = getSeason(props.lat, new Date().getMonth());
+    console.log(season);
+
     return (
         <div>Season Display</div>
     );
